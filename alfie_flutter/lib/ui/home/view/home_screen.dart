f0c68d740f20f7863743b9e4364872ccfc243387
João Samuel Diogo <joao.diogo@mindera.com>
2026-01-26 12:24:48 +0000

    added home screen input field

--- alfie_flutter/lib/ui/home/view/home_screen.dart
+++ alfie_flutter/lib/ui/home/view/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:alfie_flutter/ui/home/view_model/home_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,6 +7,27 @@ class HomeScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Center(child: Text('Hello Home Screen!'));
+    final homeState = ref.watch(homeViewModelProvider);
+    final viewModel = ref.read(homeViewModelProvider.notifier);
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              labelText: 'Enter some text',
+              border: OutlineInputBorder(),
+            ),
+            onChanged: (value) => viewModel.updateText(value),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            'Display: ${homeState.displayedText}',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ],
+      ),
+    );
   }
 }
