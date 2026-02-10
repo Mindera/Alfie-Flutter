abeb1b6ff9fc2c39ead9dd5182371472a67abb6e
João Samuel Diogo <joao.diogo@mindera.com>
2026-02-10 12:19:49 +0000

    ALFMOB-219: Upadeted Attributes Mapper

--- alfie_flutter/lib/graphql/extensions/attributes_mapper.dart
+++ alfie_flutter/lib/graphql/extensions/attributes_mapper.dart
@@ -1,12 +1,9 @@
 import 'package:alfie_flutter/graphql/generated/queries/fragments/attributes_fragment.graphql.dart';
 
 extension AttributesMapper on List<Fragment$AttributesFragment>? {
-  Map<String, String> toDomain() {
-    if (this == null) {
-      return {};
-    }
-    return Map.fromEntries(
-      this!.map((attribute) => MapEntry(attribute.key, attribute.value)),
-    );
+  Map<String, String>? toDomain() {
+    if (this == null) return null;
+
+    return {for (final attribute in this!) attribute.key: attribute.value};
   }
 }
