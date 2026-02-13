c2732d7c6f3b53398f92e9612eb44808305fc59c
João Samuel Diogo <joao.diogo@mindera.com>
2026-02-13 13:19:58 +0000

    Added corrupt ignore

--- alfie_flutter/test_coverage.sh
+++ alfie_flutter/test_coverage.sh
@@ -2,10 +2,10 @@ flutter test --coverage --branch-coverage
 
 lcov --remove coverage/lcov.info \
   'lib/graphql/generated/**' \
-  -o coverage/lcov.info --branch-coverage --ignore-errors inconsistent
+  -o coverage/lcov.info --branch-coverage --ignore-errors inconsistent,corrupt
 
 if [ "$GITHUB_ACTIONS" != "true" ]; then
-  genhtml coverage/lcov.info -o coverage/html --branch-coverage --ignore-errors inconsistent
+  genhtml coverage/lcov.info -o coverage/html --branch-coverage --ignore-errors inconsistent,corrupt
 
   open coverage/html/index.html
 fi
