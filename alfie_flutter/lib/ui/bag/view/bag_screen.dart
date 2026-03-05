998f0eefd0d508e2902982d8474b617bd6239512
João Samuel Diogo <joao.diogo@mindera.com>
2026-03-05 17:35:52 +0000

    ALFMOB-242: Removed Refresh Indicator

--- alfie_flutter/lib/ui/bag/view/bag_screen.dart
+++ alfie_flutter/lib/ui/bag/view/bag_screen.dart
@@ -17,94 +17,91 @@ class BagScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final bagItems = ref.watch(bagViewModelProvider);
 
-    return RefreshIndicator.adaptive(
-      onRefresh: () async => ref.refresh(bagViewModelProvider),
-      child: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            pinned: true,
-            leading: IconButton(
-              onPressed: () {
-                if (!context.safePop()) context.goTo(AppRoute.home);
-              },
-              icon: Icon(AppIcons.back),
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          pinned: true,
+          leading: IconButton(
+            onPressed: () {
+              if (!context.safePop()) context.goTo(AppRoute.home);
+            },
+            icon: Icon(AppIcons.back),
+          ),
+          title: Text('Bag', style: context.textTheme.headlineSmall),
+          automaticallyImplyLeading: false,
+          backgroundColor: AppColors.neutral,
+          surfaceTintColor: AppColors.transparent,
+          elevation: 0,
+        ),
+        SliverPadding(
+          padding: EdgeInsets.symmetric(
+            horizontal: Spacing.small,
+          ).add(EdgeInsets.only(bottom: Spacing.small)),
+          sliver: bagItems.isEmpty
+              ? SliverToBoxAdapter(
+                  child: const Center(child: Text('Your bag is empty.')),
+                )
+              : SliverList.separated(
+                  itemCount: bagItems.length,
+                  itemBuilder: (context, index) {
+                    return HorizontalProductCard(
+                      bagItem: bagItems[index],
+                      onDismiss: (item) {
+                        ref
+                            .read(bagViewModelProvider.notifier)
+                            .removeItem(item.product.id);
+                      },
+                      onSave: (item) {
+                        //TODO Add to wishlist
+                      },
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return const SizedBox(height: Spacing.small);
+                  },
+                ),
+        ),
+        SliverAppBar(
+          pinned: true,
+          flexibleSpace: Container(
+            padding: const EdgeInsets.all(Spacing.small),
+            height: double.maxFinite,
+            decoration: BoxDecoration(
+              color: Colors.grey.shade100,
+              border: Border(top: BorderSide(color: Colors.grey.shade300)),
             ),
-            title: Text('Bag', style: context.textTheme.headlineSmall),
-            automaticallyImplyLeading: false,
-            backgroundColor: AppColors.neutral,
-            surfaceTintColor: AppColors.transparent,
-            elevation: 0,
-          ),
-          SliverPadding(
-            padding: EdgeInsets.symmetric(
-              horizontal: Spacing.small,
-            ).add(EdgeInsets.only(bottom: Spacing.small)),
-            sliver: bagItems.isEmpty
-                ? SliverToBoxAdapter(
-                    child: const Center(child: Text('Your bag is empty.')),
-                  )
-                : SliverList.separated(
-                    itemCount: bagItems.length,
-                    itemBuilder: (context, index) {
-                      return HorizontalProductCard(
-                        bagItem: bagItems[index],
-                        onDismiss: (item) {
-                          ref
-                              .read(bagViewModelProvider.notifier)
-                              .removeItem(item.product.id);
-                        },
-                        onSave: (item) {
-                          //TODO Add to wishlist
-                        },
-                      );
-                    },
-                    separatorBuilder: (BuildContext context, int index) {
-                      return const SizedBox(height: Spacing.small);
-                    },
+            child: Column(
+              spacing: Spacing.small,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  child: AppButton.destructive(
+                    label: 'Clear Bag',
+                    onPressed: () =>
+                        ref.read(bagViewModelProvider.notifier).clearBag(),
                   ),
-          ),
-          SliverAppBar(
-            pinned: true,
-            flexibleSpace: Container(
-              padding: const EdgeInsets.all(Spacing.small),
-              height: double.maxFinite,
-              decoration: BoxDecoration(
-                color: Colors.grey.shade100,
-                border: Border(top: BorderSide(color: Colors.grey.shade300)),
-              ),
-              child: Column(
-                spacing: Spacing.small,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SizedBox(
-                    width: double.infinity,
-                    child: AppButton.destructive(
-                      label: 'Clear Bag',
-                      onPressed: () =>
-                          ref.read(bagViewModelProvider.notifier).clearBag(),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Total:',
+                      style: Theme.of(context).textTheme.titleLarge,
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Total:',
-                        style: Theme.of(context).textTheme.titleLarge,
+                    Text(
+                      '\$${ref.read(bagViewModelProvider.notifier).total.toStringAsFixed(2)}',
+                      style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                        fontWeight: FontWeight.bold,
                       ),
-                      Text(
-                        '\$${ref.read(bagViewModelProvider.notifier).total.toStringAsFixed(2)}',
-                        style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
