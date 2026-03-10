641e11d75e71ccb44b4713b60332db68a96ba941
João Samuel Diogo <joao.diogo@mindera.com>
2026-03-10 14:36:30 +0000

    ALFMOB-226: Added Snack Bar to PDP Add to Bag

--- alfie_flutter/lib/ui/product_detail/view/product_main_info.dart
+++ alfie_flutter/lib/ui/product_detail/view/product_main_info.dart
@@ -1,12 +1,16 @@
 import 'package:alfie_flutter/data/models/product.dart';
+import 'package:alfie_flutter/global_keys.dart';
+import 'package:alfie_flutter/routing/app_route.dart';
 import 'package:alfie_flutter/ui/core/themes/app_icons.dart';
 import 'package:alfie_flutter/ui/core/themes/colors.dart';
 import 'package:alfie_flutter/ui/core/themes/spacing.dart';
 import 'package:alfie_flutter/ui/core/themes/typography.dart';
 import 'package:alfie_flutter/ui/core/ui/button/app_button.dart';
 import 'package:alfie_flutter/ui/core/ui/color_swatch.dart';
+import 'package:alfie_flutter/ui/core/ui/snack_bar.dart';
 import 'package:alfie_flutter/ui/product_detail/view_model/product_detail_view_model.dart';
 import 'package:alfie_flutter/utils/build_context_extensions.dart';
+import 'package:alfie_flutter/utils/navigation_helpers.dart';
 import 'package:alfie_flutter/utils/string_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -61,6 +65,18 @@ class ProductMainInfo extends ConsumerWidget {
                   ref
                       .read(productDetailViewModelProvider(product.id).notifier)
                       .addToBag(product);
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    AppSnackBar.build(
+                      context: context,
+                      infoText: "Added to bag.",
+                      actionText: "Go to Bag",
+                      messengerKey: ref.watch(scaffoldMessengerKeyProvider),
+                      onTapAction: () {
+                        context.goTo(AppRoute.bag);
+                      },
+                    ),
+                  );
                 },
               ),
             ),
