aa2f75ba27e14b590ab93b50c8b167b2aadace71
João Samuel Diogo <joao.diogo@mindera.com>
2026-03-10 16:08:55 +0000

    ALFMOB-247: Added Wishlist View Model

---
+++ alfie_flutter/lib/ui/wishlist/view_model/wishlist_view_model.dart
@@ -0,0 +1,31 @@
+import 'package:alfie_flutter/data/models/product.dart';
+import 'package:alfie_flutter/data/repositories/wishlist_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class WishlistViewModel extends Notifier<List<Product>> {
+  late final WishlistRepository _repository;
+
+  @override
+  List<Product> build() {
+    _repository = ref.watch(wishlistRepositoryProvider);
+    return _repository.getWishlist();
+  }
+
+  Future<void> addProduct(Product item) async {
+    await _repository.addToWishlist(item);
+    state = _repository.getWishlist();
+  }
+
+  Future<void> removeProduct(String productId) async {
+    await _repository.removeFromWishlist(productId);
+    state = _repository.getWishlist();
+  }
+
+  Future<void> clearWishlist() async {
+    await _repository.clearWishlists();
+    state = [];
+  }
+}
+
+final wihslistViewModelProviders =
+    NotifierProvider<WishlistViewModel, List<Product>>(WishlistViewModel.new);
