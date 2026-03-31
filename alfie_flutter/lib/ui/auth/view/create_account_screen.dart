bdf73c651d4cb267b84944c418a98d377000b2ef
João Samuel <joao.diogo@mindera.com>
2026-03-31 14:18:11 +0100

    ALFMOB-261: Added Auth Screen

---
+++ alfie_flutter/lib/ui/auth/view/create_account_screen.dart
@@ -0,0 +1,27 @@
+import 'package:alfie_flutter/data/repositories/auth_repository.dart';
+import 'package:alfie_flutter/ui/core/ui/button/app_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CreateAccountScreen extends ConsumerWidget {
+  const CreateAccountScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: SizedBox(
+        width: double.maxFinite,
+        child: Center(
+          child: AppButton.primary(
+            label: "Create Account",
+            onPressed: () {
+              ref
+                  .read(authRepositoryProvider.notifier)
+                  .login('admin@alfie.com', 'pass');
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
