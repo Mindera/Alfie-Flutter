6a069cbc8df3bdd0563274264ceab7f1ff0b1d07
João Samuel <joao.diogo@mindera.com>
2026-04-21 17:11:54 +0100

    ALFMOB-309: Added Action to Continue Button

--- alfie_flutter/lib/ui/checkout/view/identification_screen.dart
+++ alfie_flutter/lib/ui/checkout/view/identification_screen.dart
@@ -1,3 +1,5 @@
+import 'package:alfie_flutter/data/repositories/user_repository.dart';
+import 'package:alfie_flutter/routing/app_route.dart';
 import 'package:alfie_flutter/ui/core/themes/app_icons.dart';
 import 'package:alfie_flutter/ui/core/themes/spacing.dart';
 import 'package:alfie_flutter/ui/core/themes/typography.dart';
@@ -5,14 +7,19 @@ import 'package:alfie_flutter/ui/core/ui/button/app_button.dart';
 import 'package:alfie_flutter/ui/core/ui/header.dart';
 import 'package:alfie_flutter/ui/core/ui/text_field/app_text_field.dart';
 import 'package:alfie_flutter/utils/build_context_extensions.dart';
+import 'package:alfie_flutter/utils/form_utils.dart';
 import 'package:alfie_flutter/utils/navigation_helpers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class IdentificationScreen extends StatelessWidget {
+class IdentificationScreen extends HookConsumerWidget {
   const IdentificationScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final email = useState("");
+
     return Scaffold(
       appBar: AppBar(
         flexibleSpace: Header(
@@ -43,10 +50,35 @@ class IdentificationScreen extends StatelessWidget {
                   textAlign: TextAlign.left,
                 ),
               ),
-              AppInputField("Email"),
+              AppInputField(
+                "Email",
+                onChanged: (value) {
+                  email.value = value;
+                },
+              ),
               SizedBox(
                 width: double.maxFinite,
-                child: AppButton.primary(label: "Continue", onPressed: () {}),
+                child: AppButton.primary(
+                  label: "Continue",
+                  onPressed: () {
+                    if (context.validateEmail(email.value) == null) {
+                      context.pushTo(
+                        ref
+                                .watch(userRepositoryProvider)
+                                .isEmailRegistered(email.value)
+                            ? AppRoute.signIn
+                            : AppRoute.createAccount,
+                        params: {"email": email.value},
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text("Please enter a valid email address."),
+                        ),
+                      );
+                    }
+                  },
+                ),
               ),
               GestureDetector(
                 onTap: () {},
