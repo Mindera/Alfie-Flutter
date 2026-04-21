849d10053efc8a236ef3d4aa16810b2c9e918289
João Samuel <joao.diogo@mindera.com>
2026-04-21 13:36:29 +0100

    ALFMOB-316: Added Checkout State Repository

---
+++ alfie_flutter/lib/data/repositories/checkout_state_repository.dart
@@ -0,0 +1,21 @@
+import 'package:alfie_flutter/data/services/persistent_storage_service.dart';
+import 'package:alfie_flutter/ui/checkout/view_model/checkout_state.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CheckoutStateRepository {
+  final IPersistentStorageService _storageService;
+
+  CheckoutStateRepository(this._storageService);
+
+  CheckoutState? getCheckoutState() {
+    return _storageService.getCheckoutState();
+  }
+
+  Future<void> saveCheckoutState(CheckoutState state) async {
+    await _storageService.saveCheckoutState(state);
+  }
+}
+
+final checkoutStateRepositoryProvider = Provider(
+  (ref) => CheckoutStateRepository(ref.watch(persistentStorageServiceProvider)),
+);
