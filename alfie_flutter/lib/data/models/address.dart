4ff37a979b185644dfc2eaee5c53106587ad55b4
João Samuel <joao.diogo@mindera.com>
2026-04-23 14:48:42 +0100

    ALFMOB-311: Added Navigation to Checkout Screen

--- alfie_flutter/lib/data/models/address.dart
+++ alfie_flutter/lib/data/models/address.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 class Address {
   final String country;
   final String postalCode;
@@ -32,6 +34,7 @@ class Address {
       city: city ?? this.city,
       postalCode: postalCode ?? this.postalCode,
       street: street ?? this.street,
+      addressLine2: addressLine2 ?? this.addressLine2,
     );
   }
 
@@ -40,4 +43,33 @@ class Address {
       city.isNotEmpty &&
       postalCode.isNotEmpty &&
       street.isNotEmpty;
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    log("$this : $other");
+
+    return other is Address &&
+        other.country == country &&
+        other.postalCode == postalCode &&
+        other.city == city &&
+        other.street == street &&
+        other.addressLine2 == addressLine2;
+  }
+
+  @override
+  int get hashCode =>
+      Object.hash(country, postalCode, city, street, addressLine2);
+
+  @override
+  String toString() {
+    return 'Address('
+        'country: $country, '
+        'city: $city, '
+        'postalCode: $postalCode, '
+        'street: $street, '
+        'addressLine2: $addressLine2'
+        ')';
+  }
 }
--- alfie_flutter/lib/ui/checkout/view/delivery_information_screen.dart
+++ alfie_flutter/lib/ui/checkout/view/delivery_information_screen.dart
@@ -1,4 +1,7 @@
 import 'package:alfie_flutter/data/models/address.dart';
+import 'package:alfie_flutter/data/models/user_data.dart';
+import 'package:alfie_flutter/data/repositories/auth_repository.dart';
+import 'package:alfie_flutter/routing/app_route.dart';
 import 'package:alfie_flutter/ui/checkout/view/address_fields.dart';
 import 'package:alfie_flutter/ui/checkout/view_model/checkout_view_model.dart';
 import 'package:alfie_flutter/ui/core/themes/app_icons.dart';
@@ -23,6 +26,12 @@ class DeliveryInformationScreen extends HookConsumerWidget {
 
     final Address? existingBillingAddress = ref.watch(
       checkoutViewModelProvider.select((state) => state.billingAddress),
+    );
+
+    final UserData userData = ref.watch(
+      checkoutViewModelProvider.select(
+        (state) => state.userData ?? UserData.empty(),
+      ),
     );
 
     final billingCheckbox = useState<bool>(
@@ -95,7 +104,7 @@ class DeliveryInformationScreen extends HookConsumerWidget {
 
                   CheckboxTile(
                     label: "Same as delivery address",
-                    initialValue: true,
+                    initialValue: billingCheckbox.value,
                     onChanged: (value) => billingCheckbox.value = value,
                   ),
                   if (!billingCheckbox.value)
@@ -126,9 +135,21 @@ class DeliveryInformationScreen extends HookConsumerWidget {
 
               ref
                   .read(checkoutViewModelProvider.notifier)
-                  .setBillingAddress(billingAddress.value);
+                  .setBillingAddress(
+                    billingCheckbox.value
+                        ? deliveryAddress.value
+                        : billingAddress.value,
+                  );
 
-              // context.goTo(AppRoute.deliveryInformation);
+              ref
+                  .read(authRepositoryProvider.notifier)
+                  .continueAsGuestUser(
+                    userData,
+                    deliveryAddress.value,
+                    billingAddress.value,
+                  );
+
+              context.goTo(AppRoute.checkout);
             },
           ),
         ),
